c3c2755869de5ae273f1cc27a2db9f980076668e
Kaleah <10958501@uvu.edu>
2022-02-16 22:22:36 -0700

    Array Cleanup

--- Prototype 2- Balloon Pop Game/Assets/Scripts/Score_Manager.cs
+++ Prototype 2- Balloon Pop Game/Assets/Scripts/Score_Manager.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Score_Manager : MonoBehaviour
 {
+    public int score; //keeps track of score
+    public TextMeshProUGUI scoretext; // text object to be modified
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateScoreText();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void IncreaseTextScore(int amount)
+    {
+        score += amount;
+        UpdateScoreText();
+    }
+    public void UpdateScoreText()
+    {
+        scoreText.text = "Score:" + score;
     }
 }
