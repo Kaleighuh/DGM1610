bebb4aa01c2e683c07a73dd9da9cde4b4bf8c312
Kaleah <10958501@uvu.edu>
2022-02-20 20:28:45 -0700

    Array Completion

--- Prototype 2- Balloon Pop Game/Assets/Scripts/Arrays/Arrays.cs
+++ Prototype 2- Balloon Pop Game/Assets/Scripts/Arrays/Arrays.cs
@@ -7,6 +7,8 @@ public class Arrays : MonoBehaviour
     string[] friendnames = {"Bob", "Jessica", "Jodi", "Arther", "Tod", "Kendal", "Nikki", };
     //                      {0,1,2,3,4,5,6,7}
     public int[] numbers = {23,46,77,10,42,98,63,81};
+    string[] Resources = {"Diamond", "Gold", "Steel", "Wood", "Stone", "NetherStone" };
+    public int[] Strength = {100,55,80,15,75,1000};
 
 
     // Start is called before the first frame update
@@ -16,12 +18,60 @@ public class Arrays : MonoBehaviour
         Debug.Log("former friend" + friendnames[0]);
         Debug.Log(friendnames[3]) +" says wassup as he heads to class";
         friendnames [0] = "Veronica";
-        Debug.Log("New friend" + friendnames[0]);
-        */
+        Debug.Log("New friend" + friendnames[0]); // number 1
+        
+        Debug.Log("former friend" + friendnames[6]);
+        Debug.Log(friendnames[2]) +" says that you are ugly";
+        friendnames[6] = "Kendal";
+        Debug.Log("New Friend" + friendnames[6]); //because Jodi called you ugly // number 2
+
+        Debug.Log("Former Materials for House" = Resources[15]);
+        Debug.Log(Resources[55]) +" Needs an upgrade";
+        Resources[15] = "mere wood";
+        Debug.Log("Upgraded House" + Resources[15]); // number 3
+
+        Debug.Log("Former Materials for House" = Resources[55]);
+        Debug.Log(Resources[75]) +" Needs an upgrade";
+        Resources[55] = "Gold";
+        Debug.Log("Upgraded House" + Resources[55]); // number 4
+
+        Debug.Log ("Former Materials for House" = Resources[75]);
+        Debug.Log(Resources[80]) +" I want a house of steel";
+        Resources[75] = "Stone";
+        Debug.Log("Upgraded House " + Resources[75]); // number 5
+
+        Debug.Log ("New House Upgrade" = Resources[80]);
+        Debug.Log (Resources[100]) + "Want a house out of Diamonds";
+        Resources[80] = "Steel";
+        Debug.Log("Final House Upgrade" + Resources[80]); // number 6
+
+        Debug.Log ("Former Materials for House" = Resources[100]);
+        Debug.Log(Resources[1000]) +" House out of NetherStone";
+        Resources[100] = "Diamond";
+        Debug.Log("Upgraded House " + Resources[100]); // number 7
+
+        Debug.Log ("former friend" + friendnames[4]);
+        Debug.Log (friendnames[3]) + "steals your prom dress";
+        friendnames[4] = "Arther";
+        Debug.Log("New Friend" + friendnames[4]); // number 8
+
+        Debug.Log ("former friend" + friendnames[7]);
+        Debug.Log (friendnames[1]) + "gives you flowers";
+        friendnames[7] = "Nikki";
+        Debug.Log("New Friend" + friendnames[7]); // number 9
+
+        Debug.Log ("former friend" + friendnames[5]);
+        Debug.Log (friendnames[6]) + "ruins your flowers";
+        friendnames[5] = "Tod";
+        Debug.Log("New Friend" + friendnames[5]); // number 10
+        
+ 
+
+
         foreach(int numbers in numbers)
         {
             print(numbers);
-        }
+        } */
     }
 }
     
