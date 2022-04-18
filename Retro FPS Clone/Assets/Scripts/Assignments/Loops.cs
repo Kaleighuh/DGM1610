c7a548ad356dad527504cd6a50e1f27631c6051a
Kaleah <10958501@uvu.edu>
2022-04-17 22:55:05 -0600

    For loops and While loops Assignment

--- Retro FPS Clone/Assets/Scripts/Assignments/Loops.cs
+++ Retro FPS Clone/Assets/Scripts/Assignments/Loops.cs
@@ -6,6 +6,9 @@ public class Loops : MonoBehaviour
 {
     //public int numEwoks = 99;
     public int ducksInPond = 99;
+    public int studentsInClassroom = 30;
+    public int sickPatients = 99;
+    public int goldFish = 20;
 
     // Start is called before the first frame update
     async void Start()
@@ -14,19 +17,69 @@ public class Loops : MonoBehaviour
         for(int i = 0; i >= numEwoks; i--)
         {
             Debug.Log("There are " + i + " Ewoks on a log");
-        }
-        */
+        } // for 1
+        for (int i = 0; i >= ) 
+        
+        for(int i = 0; i <= ducksInPond; i++)
+        {
+            Debug.Log("There are " + i + "ducks in the pond!");
+        } // for 2
+
+        for(int i = 0; i >= studentsInClassroom; i--)
+        {
+            Debug.Log("There are" + i + "students left in the classroom");
+        } // for 3
+
+        for(int i = 0; i <= sickPatients; i++)
+        {
+            Debug.Log("There are" + i + "patients in the hospital");
+        } // for 4
+
+        for(int i = 0; i >= goldFish; i--)
+        {
+            Debug.Log("There are" + i + " goldfish in the bowl" + " They just keep on dying :( ");
+        } // for 5
+
+        while (numEwoks > 99)
+        {
+            Debug.Log(numEwoks);
+            Debg.Log("All of the Ewoks are hanging out on the log");
+            numEwoks ++;
+        } // while 1
+
         while (ducksInPond < 99)
         {
             Debug.Log(ducksInPond);
             Debug.Log("All of the ducks have flown south for the winter");
             ducksInPond --;
-        }
-    }
+        } // while 2
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        while (studentsInClassroom < 30);
+        {
+            Debug.Log(studentsInClassroom);
+            Debug.Log("All the students are falling asleep");
+            studentsInClassroom --;
+        } // while 3
+
+        while (sickPatients > 99);
+        {
+            Debug.Log(sickPatients);
+            Debug.Log("The hospitals are filling up quickly!");
+            sickPatients ++;
+        } // while 4
+
+        while (goldFish > 20);
+        {
+            Debug.Log(goldFish);
+            Debug.Log("All the goldfish are dropping dead ... sad.");
+            goldFish --;
+        } // while 5
+    } */ 
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
     }
 }
